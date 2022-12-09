24e0973a0e9011ef074b370642d67b0d267c7796
JayAleecha <aleecha.2546@gmail.com>
2022-12-09 16:58:13 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,21 @@
 using namespace std;
 
 int main(){
+    int n;
+    int m=0;
+    int o=0;
+        while(n!=0){
     cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    cin >> n;
+
+        if(n%2==0 && n!=0){
+            m++;
+        }
+        if(n%2!=0){
+            o++;
+        }
+    }
+    cout << "#Even numbers = " << m;
+    cout << "#Odd numbers = " << o;
     return 0;
 }
